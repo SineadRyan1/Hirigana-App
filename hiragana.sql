0d6466f915c443fedaa44d30b604afd189eae5ff
Sinéad Ryan <34307457+SineadRyan1@users.noreply.github.com>
2020-10-14 15:09:16 +0100

    Add files via upload

---
+++ hiragana.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2019 at 05:30 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ajaxsr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hiragana`
+--
+
+CREATE TABLE `hiragana` (
+  `quizID` int(255) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `choice1` varchar(255) NOT NULL,
+  `choice2` varchar(255) NOT NULL,
+  `choice3` varchar(255) NOT NULL,
+  `choice4` varchar(255) NOT NULL,
+  `answer` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hiragana`
+--
+ALTER TABLE `hiragana`
+  ADD PRIMARY KEY (`quizID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hiragana`
+--
+ALTER TABLE `hiragana`
+  MODIFY `quizID` int(255) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ questions.js
@@ -0,0 +1,90 @@
+var questions = [{
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"img/a.png",
+		"option1":"o",
+		"option2": "a",
+		"option3":"u",
+		"option4":"e"
+		"answer" :"B"
+	
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"img/o.png",
+		"option1":"ko",
+		"option2": "ku",
+		"option3":"i",
+		"option4":"tu"
+		"answer": "1"
+	
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"img/e.png",
+		"option1":"mu",
+		"option2": "ka",
+		"option3":"e",
+		"option4":"ya",
+		"answer": "C"
+	
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"img/i.png",
+		"option1":"a",
+		"option2": "ku",
+		"option3":"u",
+		"option4":"i"
+		"answer" : "D"
+	
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"u.png",
+		"option1":"ko",
+		"option2": "u",
+		"option3":"i",
+		"option4":"tu"
+		"answer" : "2"
+	
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"img/ko.png",
+		"option1":"ko",
+		"option2": "ku",
+		"option3":"i",
+		"option4":"tu"
+		"answer" : "1"
+	
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"img/ki.png",
+		"option1":"ko",
+		"option2": "ku",
+		"option3":"ki",
+		"option4":"tu"
+		"answer" : "3"
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"img/ku.png",
+		"option1":"ko",
+		"option2": "tsu",
+		"option3":"ku",
+		"option4":"ka"
+		"answer": "3"
+	}, {
+		question :"What is this symbol's romanji equivalent?"
+		image_source :"img/ka.png",
+		"option1":"ko",
+		"option2": "ka",
+		"option3":"i",
+		"option4":"tu"
+		"answer" : "2"
+	}, {
+		question:"What is this symbol's romanji equivalent?"
+		image_source :"img/ke.png",
+		"option1":"ke",
+		"option1": "ku",
+		"option2":"i",
+		"option3":"tu"
+		"option4" : "u"
+		"answer" :"1";
+	
+	}];
